4c47b2e2e004a532d49ee985b972aeb548316fce
M-Picco <martin@xlabs.xyz>
2024-05-14 12:55:58 -0300

    Add solana devnet secret to inject script

--- deploy/inject-rpc-secret.sh
+++ deploy/inject-rpc-secret.sh
@@ -5,3 +5,7 @@ kubectl delete secret wormholescan-bff-rpc --ignore-not-found --namespace=wormsc
 kubectl create secret generic wormholescan-bff-rpc \
     --from-literal=SOLANA_RPC_URL=${SOLANA_RPC_URL} \
     --namespace=wormscan
+
+kubectl create secret generic wormholescan-bff-rpc \
+    --from-literal=SOLANA_DEVNET_RPC_URL=${SOLANA_DEVNET_RPC_URL} \
+    --namespace=wormscan
